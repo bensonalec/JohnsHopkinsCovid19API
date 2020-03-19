bb87f533bf8040a5ef5d2121801bc36cd9fa630b
Alec <alecbenson21@gmail.com>
2020-03-18 21:13:24 -0600

    Added API serving

--- main.go
+++ main.go
@@ -2,9 +2,81 @@ package main
 
 
 import (
-	"fmt"
+    "net/http"
+	"github.com/gorilla/mux"
+	"log"
 )
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusNotFound)
+    w.Write([]byte(`{"status code": "400"}`))
+}
+
+func stateGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var state string
+	var toWrite string
+
+	if val,ok := pathParams["state"]; ok {
+		state = val
+
+		toWrite = getState(state)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func countryGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var country string
+	var toWrite string
+
+	if val,ok := pathParams["country"]; ok {
+		country = val
+		toWrite = getCountry(country)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))	
+	}
+
+
+}
+
+func countrystateGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var country string
+	var state string
+	var toWrite string
+
+	if val,ok := pathParams["state"]; ok {
+		state = val
+		if val,ok := pathParams["country"]; ok {
+			country = val
+			toWrite = getCountryState(state,country)
+			w.WriteHeader(200)
+			w.Write([]byte((toWrite)))	
+	
+		}
+	}
+
+
+}
+
+
 func main() {
-	fmt.Println(getCountryState("Washington","US"))
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/").Subrouter()
+
+	api.HandleFunc("/State/{state}",stateGET).Methods(http.MethodGet)
+	api.HandleFunc("/Country/{country}",countryGET).Methods(http.MethodGet)
+	api.HandleFunc("/CountryAndState/{state}/{country}",countrystateGET).Methods(http.MethodGet)
+	api.HandleFunc("/", notFound)
+	log.Fatal(http.ListenAndServe(":80", r))
+
 }
