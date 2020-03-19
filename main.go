93069af524371d1c3fba79e0be736062809dc691
Alec <alecbenson21@gmail.com>
2020-03-18 23:16:02 -0600

    Added boilerplate for endpoints

--- main.go
+++ main.go
@@ -15,9 +15,19 @@ func main() {
 	api.HandleFunc("/Daily/Country/{country}",countryGET).Methods(http.MethodGet)
 	api.HandleFunc("/Daily/CountryAndState/{state}/{country}",countrystateGET).Methods(http.MethodGet)
 	api.HandleFunc("/Daily",dailyallGET).Methods(http.MethodGet)
+
 	api.HandleFunc("/Timeseries/Confirmed",TSallConfirmedGET).Methods(http.MethodGet)
 	api.HandleFunc("/Timeseries/Recovered",TSallRecoveredGET).Methods(http.MethodGet)
 	api.HandleFunc("/Timeseries/Deaths",TSallDeathsGET).Methods(http.MethodGet)
+
+	// api.HandleFunc("/Timeseries/Confirmed/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
+	// api.HandleFunc("/Timeseries/Recovered/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
+	// api.HandleFunc("/Timeseries/Deaths/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
+
+	// api.HandleFunc("/Timeseries/Confirmed/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
+	// api.HandleFunc("/Timeseries/Recovered/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
+	// api.HandleFunc("/Timeseries/Deaths/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
+
 	api.HandleFunc("/", notFound)
 	log.Fatal(http.ListenAndServe(":80", r))
 
