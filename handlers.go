6d12f366c51a84ebf0d3cfa5716450e333b0cfac
Alec <alecbenson21@gmail.com>
2020-03-18 23:31:04 -0600

    Added endpoints for time series by state and country

--- handlers.go
+++ handlers.go
@@ -112,3 +112,111 @@ func TSallDeathsGET(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte((toWrite)))	
 	
 }
+
+func TSConfirmedstateGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var state string
+	var toWrite string
+
+	if val,ok := pathParams["state"]; ok {
+		state = val
+
+		toWrite = getTSConfirmedState(state)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func TSRecoveredstateGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var state string
+	var toWrite string
+
+	if val,ok := pathParams["state"]; ok {
+		state = val
+
+		toWrite = getTSRecoveredState(state)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func TSDeathsstateGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var state string
+	var toWrite string
+
+	if val,ok := pathParams["state"]; ok {
+		state = val
+
+		toWrite = getTSDeathsState(state)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func TSConfirmedcountryGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var country string
+	var toWrite string
+
+	if val,ok := pathParams["country"]; ok {
+		country = val
+
+		toWrite = getTSConfirmedCountry(country)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func TSRecoveredcountryGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var country string
+	var toWrite string
+
+	if val,ok := pathParams["country"]; ok {
+		country = val
+
+		toWrite = getTSRecoveredCountry(country)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
+
+func TSDeathscountryGET(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	var country string
+	var toWrite string
+
+	if val,ok := pathParams["country"]; ok {
+		country = val
+
+		toWrite = getTSDeathsCountry(country)
+		w.WriteHeader(200)
+		w.Write([]byte((toWrite)))
+
+	}
+
+
+}
--- main.go
+++ main.go
@@ -17,16 +17,15 @@ func main() {
 	api.HandleFunc("/Daily",dailyallGET).Methods(http.MethodGet)
 
 	api.HandleFunc("/Timeseries/Confirmed",TSallConfirmedGET).Methods(http.MethodGet)
-	api.HandleFunc("/Timeseries/Recovered",TSallRecoveredGET).Methods(http.MethodGet)
 	api.HandleFunc("/Timeseries/Deaths",TSallDeathsGET).Methods(http.MethodGet)
 
-	// api.HandleFunc("/Timeseries/Confirmed/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
-	// api.HandleFunc("/Timeseries/Recovered/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
-	// api.HandleFunc("/Timeseries/Deaths/State/{state}",TSallDeathsGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Confirmed/State/{state}",TSConfirmedstateGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Recovered/State/{state}",TSRecoveredstateGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Deaths/State/{state}",TSDeathsstateGET).Methods(http.MethodGet)
 
-	// api.HandleFunc("/Timeseries/Confirmed/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
-	// api.HandleFunc("/Timeseries/Recovered/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
-	// api.HandleFunc("/Timeseries/Deaths/Country/{country}",TSallConfirmedGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Confirmed/Country/{country}",TSConfirmedcountryGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Recovered/Country/{country}",TSRecoveredcountryGET).Methods(http.MethodGet)
+	api.HandleFunc("/Timeseries/Deaths/Country/{country}",TSDeathscountryGET).Methods(http.MethodGet)
 
 	api.HandleFunc("/", notFound)
 	log.Fatal(http.ListenAndServe(":80", r))
